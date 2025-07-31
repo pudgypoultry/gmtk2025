017b3cb007f3e21e4d270061cfacafceab578170
Cory Johns <cory@triplevisiongroup.com>
2025-07-31 15:17:37 -0700

    Refactored ico_layer_mover to use collision layers

--- Scenes/pillar_control.gd
+++ Scenes/pillar_control.gd
@@ -11,22 +11,26 @@ extends Node3D
 func _input(event) -> void:
 	if event.is_action_pressed("Toggle Pillar L1"):
 		#var obj:Node3D = await FindTargetPillar(shell_detection_ray, 1)
-		var obj:Node3D = FindNearestPillar(pillar_set_L1)
+		var obj:Node3D = await FindTargetPillarByLayer(shell_detection_ray, 1)
+		#var obj:Node3D = FindNearestPillar(pillar_set_L1)
 		if obj:
 			pillar_set_L1.MoveRadially(obj)
 	elif event.is_action_pressed("Toggle Pillar L2"):
 		#var obj:Node3D = await FindTargetPillar(shell_detection_ray, 2)
-		var obj:Node3D = FindNearestPillar(pillar_set_L2)
+		var obj:Node3D = await FindTargetPillarByLayer(shell_detection_ray, 2)
+		#var obj:Node3D = FindNearestPillar(pillar_set_L2)
 		if obj:
 			pillar_set_L2.MoveRadially(obj)
 	elif event.is_action_pressed("Toggle Pillar L3"):
 		#var obj:Node3D = await FindTargetPillar(shell_detection_ray, 3)
-		var obj:Node3D = FindNearestPillar(pillar_set_L3)
+		var obj:Node3D = await FindTargetPillarByLayer(shell_detection_ray, 3)
+		#var obj:Node3D = FindNearestPillar(pillar_set_L3)
 		if obj:
 			pillar_set_L3.MoveRadially(obj)
 	elif event.is_action_pressed("Toggle Pillar L4"):
 		#var obj:Node3D = await FindTargetPillar(shell_detection_ray, 4)
-		var obj:Node3D = FindNearestPillar(pillar_set_L4)
+		var obj:Node3D = await FindTargetPillarByLayer(shell_detection_ray, 4)
+		#var obj:Node3D = FindNearestPillar(pillar_set_L4)
 		if obj:
 			pillar_set_L4.MoveRadially(obj)
 		
@@ -40,76 +44,30 @@ func FindNearestPillar(pillar_set) -> Node3D:
 			target = pillar
 	print("found target: " + target.name)
 	return target
-	
-# uses input RayCast3D to find the wall of the shell,
-# then uses the normal of the wall to find the pillar of the given set number
-func FindTargetPillar(ray:RayCast3D, set_number:int) -> Node3D:
-	# https://forum.godotengine.org/t/possible-to-detect-multiple-collisions-with-raycast2d/27326
+
+
+func FindTargetPillarByLayer(ray:RayCast3D, set_number:int, layer_int:int=12) -> Node3D:
 	var shell_normal:Vector3
 	# look for the shell in raycast
-	var all_collisions = []
-	while ray.is_colliding():
+	if ray.is_colliding():
 		var obj = ray.get_collider()
 		# check if shell has been found
-		print("Raycast Shell Find: " + obj.get_parent().name)
+		#print("Raycast Shell Find: " + obj.get_parent().name)
 		if obj.get_parent().name == shell_node.get_child(0).get_child(0).name:
 			# get the normal direction on shell
 			shell_normal = ray.get_collision_normal()
-			break
-		# continue with search
-		all_collisions.append(obj)
-		ray.add_exception(obj)
-		ray.force_raycast_update()
-	
-	# clear exceptions
-	for obj in all_collisions:
-		ray.remove_exception( obj )
-		
-	# look for column meshes with a ray from origin in -normal direction
-	if shell_normal:
-		var excluded_meshes = []
-		if set_number == 1:
-			excluded_meshes = find_all(pillar_set_L2, StaticBody3D)
-			excluded_meshes.append_array(find_all(pillar_set_L3, StaticBody3D))
-			excluded_meshes.append_array(find_all(pillar_set_L4, StaticBody3D))
-		if set_number == 2:
-			excluded_meshes = find_all(pillar_set_L1, StaticBody3D)
-			excluded_meshes.append_array(find_all(pillar_set_L3, StaticBody3D))
-			excluded_meshes.append_array(find_all(pillar_set_L4, StaticBody3D))
-		if set_number == 3:
-			excluded_meshes = find_all(pillar_set_L1, StaticBody3D)
-			excluded_meshes.append_array(find_all(pillar_set_L2, StaticBody3D))
-			excluded_meshes.append_array(find_all(pillar_set_L4, StaticBody3D))
-		if set_number == 4:
-			excluded_meshes = find_all(pillar_set_L1, StaticBody3D)
-			excluded_meshes.append_array(find_all(pillar_set_L2, StaticBody3D))
-			excluded_meshes.append_array(find_all(pillar_set_L3, StaticBody3D))
-		
-		#excluded_meshes.append_array(find_all(player_node, StaticBody3D))
-		excluded_meshes.append_array(find_all(shell_node, StaticBody3D))
-		
-		print("Excluded Meshes: ", excluded_meshes.size())
-		# check the physics process directly for the temporary raycast
-		var space_state = get_world_3d().direct_space_state
-		# check from the origin in the direction of -shell_normal
-		var query = PhysicsRayQueryParameters3D.create(Vector3.ZERO, -shell_normal * 100)
-		# skip all the meshes we are not looking for
-		#await get_tree().create_timer(10).timeout
-		query.exclude = excluded_meshes
-		# activate the ray cast
-		var result = space_state.intersect_ray(query)
-		if result:
-			print("Found Pillar: " + result.collider.get_parent().name)
-			# return pillar matching set
-			return result.collider.get_parent()
-	return null
 
-# https://www.reddit.com/r/godot/comments/16kkpo2/finding_child_node_by_type/
-func find_all(parent, type, node_array:Array[Node]=[]) -> Array[Node]:
-	for child in parent.get_children():
-		if is_instance_of(child, type):
-			node_array.append(child)
-		var grandchildern:Array = find_all(child, type)
-		if grandchildern.size() > 0:
-			node_array.append_array(grandchildern)
-	return node_array
+	# check the physics process directly for the temporary raycast
+	var space_state = get_world_3d().direct_space_state
+	# check from the origin in the direction of -shell_normal
+	var query = PhysicsRayQueryParameters3D.create(Vector3.ZERO, -shell_normal * 100)
+	# set layer mask layer to set_number + layer_int
+	query.set_collision_mask(pow(2, set_number+layer_int-1))
+	#await get_tree().create_timer(10).timeout
+	# activate the ray cast
+	var result = space_state.intersect_ray(query)
+	if result:
+		print("Found Pillar: " + result.collider.get_parent().name)
+		# return pillar matching set
+		return result.collider.get_parent()
+	return null
--- ico_layer_mover.gd
+++ ico_layer_mover.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var inactive_distance:float = 10.0
 @export var icosphere_layer: Node3D
+@export var layer:int
 
 enum pillar_state {IN, OUT, MOVING}
 
@@ -18,6 +19,7 @@ var pillars:Dictionary[String, PillarContatiner] = {}
 func _ready() -> void:
 	for child:Node3D in icosphere_layer.get_children():
 		pillars[child.name] = PillarContatiner.new(pillar_state.IN, child)
+		child.get_child(0).set_collision_layer_value(12 + layer, true)
 		MoveRadially(child, Vector3.ZERO, false)
 
 func MoveRadially(obj:Node3D, origin:Vector3=Vector3.ZERO, animate:bool=true) -> void:
